0d2cdf5c51b46449c6c4c23a581c033ded12e6a2
Benjamin Num <benjaminnum@Benjamins-MacBook-Pro.local>
2023-09-29 19:26:31 +0100

    aggregation fix

--- api/controllers.py
+++ api/controllers.py
@@ -23,12 +23,10 @@ def post_items():
 def get_inventory():
     # Return the list of items
     # Return the item name and total quantity
+
     pipeline = [
-    {"$unwind": "$items"},
-    {"$group": {"_id": "$items", "inventory": {"$sum": "$inventory"}}},
-    {"$project": {"_id": 0, "items": "$_id", "inventory": "$inventory"}},
+        {"$unwind": "$name"},
+        {"$group": {"_id": "$name", "quantity": {"$sum": "$quantity"}}},
+        {"$project": {"_id": 0, "name": "$_id", "quantity": "$quantity"}},
     ]
-    return jsonify(list(db.mongo.db.collection.aggregate(pipeline)))
-
-    # return jsonify(list(db.mongo.db.schema.find({}, {"_id": False})))
-
+    return jsonify(list(db.mongo.db.schema.aggregate(pipeline)))
