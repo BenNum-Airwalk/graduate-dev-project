9277d193cf52063c1ad16c598632bc602ac4412a
Benjamin Num <benjaminnum@Benjamins-MBP.medusa.local>
2023-09-29 16:09:03 +0100

    added a comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,10 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+
 );
+
+//a comment
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
